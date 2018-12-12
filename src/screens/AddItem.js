5cf194272e36d870de5c820ab8425c05e5a0455f
Anne <anne-minkenberg@web.de>
2018-12-13 00:59:44 +0100

    add AddButton+input

--- src/screens/AddItem.js
+++ src/screens/AddItem.js
@@ -1,32 +1,91 @@
 import React, { Component } from 'react';
 import {
-  StyleSheet,
+  View,
   Text,
-  View
+  StyleSheet,
+  TextInput,
+  TouchableHighlight
 } from 'react-native';
 
 export default class AddItem extends Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+        name: '',
+        error: false
+      }
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleChange(e) {
+      this.setState({
+        name: e.nativeEvent.text
+      });
+    }
+    handleSubmit() {
+      console.log(this.state.name)
+    }
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.title}>
-          Add Item
-        </Text>
+      <View style={styles.main}>
+        <Text style={styles.title}>Add Item</Text>
+        <TextInput
+              style={styles.itemInput}
+              onChange={this.handleChange}
+            />
+        <TouchableHighlight
+                style = {styles.button}
+                underlayColor= "white"
+                onPress = {this.handleSubmit}
+              >
+              <Text
+                  style={styles.buttonText}>
+                  Add
+              </Text>
+            </TouchableHighlight>
       </View>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  main: {
     flex: 1,
+    padding: 30,
+    flexDirection: 'column',
     justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#2a8ab7'
   },
   title: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    marginBottom: 20,
+    fontSize: 25,
+    textAlign: 'center'
+  },
+  itemInput: {
+    height: 50,
+    padding: 4,
+    marginRight: 5,
+    fontSize: 23,
+    borderWidth: 1,
+    borderColor: 'white',
+    borderRadius: 8,
+    color: 'white'
+  },
+  buttonText: {
+    fontSize: 18,
+    color: '#111',
+    alignSelf: 'center'
+  },
+  button: {
+    height: 45,
+    flexDirection: 'row',
+    backgroundColor:'white',
+    borderColor: 'white',
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 10,
+    marginTop: 10,
+    alignSelf: 'stretch',
+    justifyContent: 'center'
   }
 });
